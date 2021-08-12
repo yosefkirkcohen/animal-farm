f8d339734bd20e556e0f54b1171f959b2c1e05c0
yosefkirkcohen <ycohen45@yahoo.com>
2021-08-12 09:07:15 -0700

    add animal name change

--- app.js
+++ app.js
@@ -6,12 +6,15 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+const animalName = document.getElementById("animal-name")
+
 const catPhoto = document.getElementById('cat-photo')
 const catSound = document.getElementById('cat-sound')
 
 
 catPhoto.addEventListener('click', () => {
-        catSound.play()
+        catSound.play();
+        animalName.textContent = "cat";
 })
 
 const dogPhoto = document.getElementById('dog-photo')
@@ -20,6 +23,7 @@ const dogSound = document.getElementById('dog-sound')
 
 dogPhoto.addEventListener('click', () => {
         dogSound.play()
+        animalName.textContent = "dog";
 })
 
 const horsePhoto = document.getElementById('horse-photo')
@@ -28,4 +32,8 @@ const horseSound = document.getElementById('horse-sound')
 
 horsePhoto.addEventListener('click', () => {
         horseSound.play()
+        animalName.textContent = "horse";
 })
+
+
+
