eb92d8d5f40d23de9d7be116c56141b39a12b873
yosefkirkcohen <ycohen45@yahoo.com>
2021-08-11 17:54:29 -0700

    first working page

--- app.js
+++ app.js
@@ -6,3 +6,26 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+const catPhoto = document.getElementById('cat-photo')
+const catSound = document.getElementById('cat-sound')
+
+
+catPhoto.addEventListener('click', () => {
+        catSound.play()
+})
+
+const dogPhoto = document.getElementById('dog-photo')
+const dogSound = document.getElementById('dog-sound')
+
+
+dogPhoto.addEventListener('click', () => {
+        dogSound.play()
+})
+
+const horsePhoto = document.getElementById('horse-photo')
+const horseSound = document.getElementById('horse-sound')
+
+
+horsePhoto.addEventListener('click', () => {
+        horseSound.play()
+})
